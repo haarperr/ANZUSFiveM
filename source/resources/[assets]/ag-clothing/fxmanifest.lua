9a6bd5b77e23dc3e5f7802ed44657af3b15db668
Tiny <dump@kerminal.net>
2023-07-15 00:20:59 -0400

    :)

---
+++ source/resources/[assets]/ag-clothing/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_f_freemode_01_anzus.meta',
+    'mp_m_freemode_01_anzus.meta',
+    'mp_f_freemode_01_anzus_factions.meta',
+    'mp_m_freemode_01_anzus_factions.meta',
+    'mp_m_freemode_01_mp_m_nm1.meta',
+
+ 
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_anzus.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_anzus.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_anzus_factions.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_anzus_factions.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_nm1.meta'
+
+
